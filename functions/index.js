02e5fce901e08dadc900455480993aa2fb1d3c4b
Christinaheaney066 <57994688+Christinaheaney066@users.noreply.github.com>
2021-04-27 11:34:18 +0100

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -7,3 +7,31 @@ const functions = require("firebase-functions");
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+//post comments
+exports.postcomments= functions.https.onRequest((request,response) => {
+return admin.firestore().collection('comments').add(request.body).then(()=>{
+    response.send("Saved in the database");
+});
+  
+  //get comments
+  exports.getcomments = functions.https.onRequest((request, response) => {
+
+let myData = []
+admin.firestore().collection('comments').get().then((snapshot) => {
+                    if (snapshot.empty) {
+                    console.log('No matching documents.');
+                    response.send('No data in database');
+                    return;
+            }
+            snapshot.forEach(doc => {
+            myData.push(doc.data());
+            });
+            // 2. Send data back to client
+            response.send(myData);
+            })
+
+  
+});
