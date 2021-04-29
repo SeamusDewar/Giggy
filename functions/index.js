2e302e9f27d75df04b3a0d75425f4cee5f101612
heatherrfox <79642038+heatherrfox@users.noreply.github.com>
2021-04-29 13:39:44 +0100

    added authorizedendpoint function

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,5 @@
 
 const functions = require("firebase-functions");
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
 const admin = require('firebase-admin');
 const cors = require('cors')({origin: true});
 admin.initializeApp();
@@ -18,11 +10,9 @@ exports.postcomments= functions.https.onRequest((request,response) => {
     cors(request, response, () => {
        const currentTime = admin.firestore.Timestamp.now();
         request.body.timestamp = currentTime;
-
-
     
-return admin.firestore().collection('comments').add(request.body).then(()=>{
-    response.send("Saved in the database");
+        return admin.firestore().collection('comments').add(request.body).then(()=>{
+        response.send("Saved in the database");
 });
   
   //get comments
@@ -66,7 +56,39 @@ admin.firestore().collection("comments").doc(request.query.id).update().then(fun
 response.send("Document successfully updated!");
 })
 });
+});
+        
+exports.authorizedendpoint = functions.https.onRequest((request, response) => {
+    cors(request, response,() => {
 
+        console.log('Check if request is authorized with Firebase ID token');
+        if ((!request.headers.authorization || !request.headers.authorization.startsWith('Bearer'))){
+            console.error('No Firebase ID token was passed as a Bearer token in the Authorization header.',
+                'Make sure you authorize your request by providing the following HTTP header:',
+                'Authorization: Bearer <Firebase ID Token>');
+            response.status(403).send('Unauthorized');
+            return;
+        }
+        let idToken;
+        if (request.headers.authorization && request.headers.authorization.startsWith('Bearer')){
+            console.log('Found "Authorization" header');
+            // Read the ID Token from the Authorization header.
+            idToken = request.headers.authorization.split('Bearer')[1];
+        } else{
+            // No cookie
+            response.status(403).send('Unauthorized');
+            return;
+        }
 
-  
+        try {
+            const decodedIdToken = admin.auth().verifyIdToken(idToken).then((token) => {
+                console.log('ID Token correctly decoded', token);
+				response.send("welcome to acts page"+token);
+                });
+        } catch (error) {
+            console.error('Error while verifying Firebase ID token:', error);
+            response.status(403).send('Unauthorized');
+            return;
+        }
+    });
 });
